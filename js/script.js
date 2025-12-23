fa60b2a5208a3ed0416fdd693fd1f9eeeef9e877
daudibrahimdev <daudibrahim.dev@gmail.com>
2025-12-23 16:38:26 +0700

    search form dinamis

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// Toggle class active
+// Toggle class active untuk navbr jadi sidebar
 
 const navbarNav = document.querySelector('.navbar-nav');
 // ketika hamburger di klik
@@ -6,13 +6,32 @@ document.querySelector('#hamburger-menu').onclick = () => {
   navbarNav.classList.toggle('active');
 };
 
+
+// toggle class active untuk search form
+
+const searchForm = document.querySelector('.search-form');
+const searchBox = document.querySelector('#search-box');
+
+document.querySelector('#search-button').onclick = (e) => {
+  searchForm.classList.toggle('active');
+  searchBox.focus(); // biar langsung active ngetik pas di klik
+  e.preventDefault(); // biar gak scroll ke# pas klik
+}
+
+
 // fungsi untuk hilangin sidebar tanpa harus klik hamburger (klik selain hambur = closed sidebar)
+// dan untuk elemen lainnya ditampung disini
 
 const hamburger = document.querySelector('#hamburger-menu');
+const searchButton = document.querySelector('#search-button');
  
 document.addEventListener('click', function(e){
   if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove('active');
+  }
+
+  if(!searchButton.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove('active');
   }
 
 })
